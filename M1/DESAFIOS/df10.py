bf2e6f87fe0999c7450cab638fb480af8e1bec7f
Manoel Gosalan <mgosalan.dev@gmail.com>
2025-07-20 21:35:55 +0100

    desafio 10

--- M1/DESAFIOS/df10.py
+++ M1/DESAFIOS/df10.py
@@ -2,46 +2,37 @@
 
 import requests
 
-def obter_cotacao(base = "EUR", destino = "JPY", chave_api="82fad69e4cedf698b95653b3f39e57b7"):
-  url = "https://api.currencystack.io/currency"
-  params = {
-    "base": base,
-    "target": destino,
-    "apikey": chave_api
-  }
-  
-  try:
-    resposta = requests.get(url, params=params)
-    dados = resposta.json()
-    
-    if 'error' in dados:
-      print(f"Erro na API: {dados['error']}")
-      return None
-    
-    taxa = dados['data']['target_values']
+def obter_cotacao(base="EUR", destino="JPY"):
+    url = f"https://api.frankfurter.app/latest?from={base}&to={destino}"
+    try:
+        resposta = requests.get(url)
+        dados = resposta.json()
+        
+        taxa = dados["rates"][destino]
+        
+        return taxa
+    except Exception as e:
+        print("❌ Erro ao pegar cotação:", e)
+        return None
 
-    print(f"🪙 1{base} = {taxa:.4f} {destino}")
-    return taxa
-  
-  except Exception as e:
-    print("❌ Erro ao pegar cotação:", e)
-    return None
-  
 def converter(valor, taxa):
     return valor * taxa
-  
-  
+
 def main():
-    chave_api = "82fad69e4cedf698b95653b3f39e57b7"
-    print("\n💱 conversor de moeda (EUR → JPY)\n")
-    
-    valor_euro = float(input("Digite o valor em Euro (EUR): "))
-    taxa = obter_cotacao("EUR", "JPY", chave_api)
-    
+    print("\n💱 Conversor de Moeda (EUR → JPY)\n")
+    try:
+        valor = float(input("Digite o valor em Euro (EUR): "))
+    except ValueError:
+        print("❌ Valor inválido.")
+        return
+
+    taxa = obter_cotacao("EUR", "JPY")
     if taxa:
-      valor_iene = converter(valor_euro, taxa)
-      print(f"\n💶 {valor_euro:.2f} EUR = {valor_iene:.2f} JPY ")
-    
-    
+        convertido = converter(valor, taxa)
+        print(f"\n💶 {valor:.2f} EUR = {convertido:.2f} JPY")
+
 if __name__ == "__main__":
     main()
+
+
+
